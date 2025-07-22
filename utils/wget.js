24937f77a289cac1c559d3def951a46f5cbc645a
Ed Chen <esc38372@gmail.com>
2025-07-22 09:20:15 -0700

    updating wget file list

--- utils/wget.js
+++ utils/wget.js
@@ -1,21 +1,24 @@
-/** @param {NS} ns */
 
+/** @param {NS} ns */
 // usage: wget "" utils/wget.js;run utils/wget.js
 export async function main(ns) {
 	const githubBase = 'https://raw.githubusercontent.com/esc2345/bitburner/refs/heads/main/';
   const files = [
-    'stats.js',
-    'scan-root.js',
-    'loop.js',
-    'connect.js',
     'buy-servers.js',
+    'connect.js',
+    'loop.js',
+    'prep.js',
+    'scan-root.js',
+    'share-all.js',
+    'start.js',
+    'stats.js',
     'scripts/grow.js',
     'scripts/hack.js',
-    'scripts/weaken.js',
     'scripts/share.js',
-    'utils/functions.js',
-    'utils/startup.js',
-    'utils/startup1.js'
+    'scripts/weaken.js',
+    'utils/1.js',
+    'utils/1a.js',
+    'utils/functions.js'
   ]
   for (let f of files) {
     let url = githubBase + f;
