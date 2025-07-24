d0b5abd5ad1586aa4dc65f88323a53f319268c01
Ed Chen <esc38372@gmail.com>
2025-07-23 22:26:50 -0700

    fixed utils/wget.js and README.md

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # bitburner
 
-wget "https://raw.githubusercontent.com/esc2345/bitburner/refs/heads/main/utils/wget.js" utils/wget.js;run utils/wget.js
+wget "https://raw.githubusercontent.com/esc2345/bitburner/refs/heads/main/utils/wget.js" utils/wget.js
 
-run utils/startup.js
+run utils/wget.js
 
+run start.js
 
--- utils/wget.js
+++ utils/wget.js
@@ -4,18 +4,21 @@
 export async function main(ns) {
 	const githubBase = 'https://raw.githubusercontent.com/esc2345/bitburner/refs/heads/main/';
   const files = [
-    'stats.js',
-    'scan-root.js',
-    'loop.js',
-    'connect.js',
     'buy-servers.js',
+    'connect.js',
+    'loop.js',
+    'prep.js',
+    'scan-root.js',
+    'share-all.js',
+    'start.js',
+    'stats.js',
     'scripts/grow.js',
     'scripts/hack.js',
     'scripts/weaken.js',
     'scripts/share.js',
-    'utils/functions.js',
-    'utils/startup.js',
-    'utils/startup1.js'
+    'utils/1.js',
+    'utils/1a.js',
+    'utils/functions.js'
   ]
   for (let f of files) {
     let url = githubBase + f;
