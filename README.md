3384443ba3d5b1067e8cb2e685c28e2f5e22bc15
Ed Chen <esc38372@gmail.com>
2025-07-24 11:56:34 -0700

    moving steps from start.js to readme.md

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ run utils/wget.js
 
 run start.js
 
+
+
+* Learn algorithms at rothman university until hack level 10
+* scan-root and loop joesguns
+* Train combat skills until level 40
+* Mug until $200k and buy tor router
+* Mug until $500k and buy brutessh
+* Mug until $1.010m and upgrade home server
+* Learn algorithms until hack 60
+* scan-root and backdoor CSEC
+* Hack for CSEC until 18.750k rep
+* Hack for Tian Di Hui until 6.250k rep
+* Buy Tian Di Hui augmentations: S.N.A, ADR-V1
+* Buy all CSEC augmentations
+* buy -a
+* scan-root and backdoor avmnite-04h
--- start.js
+++ start.js
@@ -21,22 +21,4 @@ export async function main(ns) {
       await ns.sleep(100);
     }
   }
-
-  ns.tprint(`
-* * * Learn algorithms at rothman university until hack level 10
-* * * scan-root and loop joesguns
-* * * Train combat skills until level 40
-* * * Mug until \$200k and buy tor router
-* * * Mug until \$500k and buy brutessh
-* * * Mug until \$1.010m and upgrade home server
-* * * Learn algorithms until hack 60
-* * * scan-root and backdoor CSEC
-* * * Hack for CSEC until 18.750k rep
-* * * Hack for Tian Di Hui until 6.250k rep
-* * * Buy Tian Di Hui augmentations: S.N.A, ADR-V1
-* * * Buy all CSEC augmentations
-* * * buy -a
-* * * scan-root and backdoor avmnite-04h
-  `);
-
 }
